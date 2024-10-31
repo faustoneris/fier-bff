9fdd9da11b94c291a981df1bb4f73b4b72f39570
Fausto neris <fausto2102@hotmail.com>
2024-10-31 13:27:58 -0300

    fix; tratativas de erro

--- src/modules/products/customers/repositories/customers.repository.ts
+++ src/modules/products/customers/repositories/customers.repository.ts
@@ -6,67 +6,42 @@ import { Feedback } from "../../models/feedback.model";
 
 @Injectable()
 export class CustomerRepository {
-    async fetchProducts(): Promise<Products[]> {
-        try {
-            const request = await axios.get(
-                `${API_BASE_URL.PRODUCTS_CUSTOMER}`
-            );
-            return request.data;
+  async fetchProducts(): Promise<Products[]> {
+    const request = await axios
+      .get(`${API_BASE_URL.PRODUCTS_CUSTOMER}`)
+      .catch((x) => {
+        throw x.response.data;
+      });
+    return request.data;
+  }
 
-        } catch (error) {
-            console.log(
-                `Erro na requisição de produtos: ${error}`
-            );
-            throw error;
+  async fetchProductsById(id: string): Promise<Products> {
+    const request = await axios
+      .get(`${API_BASE_URL.PRODUCTS_CUSTOMER}/${id}`)
+      .catch((x) => {
+        throw x.response.data;
+      });
+    return request.data;
+  }
 
-        }
-    }
+  async fetchProductsByCategory(category: string): Promise<Products[]> {
+    const request = await axios
+      .get(`${API_BASE_URL.PRODUCTS_CUSTOMER}/categories/${category}`)
+      .catch((x) => {
+        throw x.response.data;
+      });
+    return request.data;
+  }
 
-    async fetchProductsById(id: string): Promise<Products> {
-        try {
-            const request = await axios.get(
-                `${API_BASE_URL.PRODUCTS_CUSTOMER}/${id}`
-            );
-            return request.data;
-
-        } catch (error) {
-            console.log(
-                `Erro na procura por id do produto: ${error}`
-            );
-            throw error;
-
-        }
-    }
-
-    async fetchProductsByCategory(category: string): Promise<Products[]>{
-        try {
-            const request = await axios.get(
-                `${API_BASE_URL.PRODUCTS_CUSTOMER}/categories/${category}`
-            );
-            return request.data;
-
-        } catch (error) {
-            console.log(
-                `Erro na procura por categoria de produto: ${error}`
-            );
-            throw error;
-
-        }
-    }
-
-    async createFeedback(productId: string, feedback: Feedback): Promise<boolean>{
-        try {
-            const request = await axios.put(
-                `${API_BASE_URL.PRODUCTS_CUSTOMER}/feedback/${productId}`,
-                feedback
-            );
-            return request.data;
-
-        } catch (error) {
-            console.log(
-                `Erro no envio de feedback: ${error}`
-            );
-            throw error;
-        }
-    }
+  async createFeedback(
+    productId: string,
+    feedback: Feedback,
+  ): Promise<boolean> {
+    const request = await axios
+      .put(`${API_BASE_URL.PRODUCTS_CUSTOMER}/feedback/${productId}`, feedback)
+      .catch((x) => {
+        throw x.response.data;
+      });
+    return request.data;
+  }
 }
--- src/modules/users/repositories/users.repository.ts
+++ src/modules/users/repositories/users.repository.ts
@@ -1,4 +1,4 @@
-import { BadGatewayException, Injectable } from "@nestjs/common";
+import { Injectable } from "@nestjs/common";
 import { UserDto } from "../models/users.dto";
 import axios from "axios";
 import { API_BASE_URL } from "src/envoriments/envoriments";
@@ -6,77 +6,54 @@ import { User } from "../login/models/user.model";
 
 @Injectable()
 export class UserRepository {
-    async findAllSuppliers(): Promise<UserDto[]> {
-        try{
-            const request = await axios.get(
-                `${API_BASE_URL.USERS}/users`
-            );
-            return request.data;
-        } catch (error) {
-            console.log(
-                `Erro na busca de fornecedores: ${error}`
-            );
-            throw error;
-        }
-    }
+  async findAllSuppliers(): Promise<UserDto[]> {
+    const request = await axios
+      .get(`${API_BASE_URL.USERS}/users`)
+      .catch((x) => {
+        throw x.response.data;
+      });
+    return request.data;
+  }
 
-    async findUserByDocument(document: string): Promise<UserDto> {
-        try {
-            const request = await axios.get(
-                `${API_BASE_URL.USERS}/users/document/${document}`
-            );
-            return request.data; 
-        } catch (error) {
-            console.log(
-                `Erro ao retornar usuario pelo documento: ${error}`
-            );
-            throw error;
-        }
-    }
+  async findUserByDocument(document: string): Promise<UserDto> {
+    const request = await axios
+      .get(`${API_BASE_URL.USERS}/users/document/${document}`)
+      .catch((x) => {
+        throw x.response.data;
+      });
+    return request.data;
+  }
 
-    async findUserByEmail(email: string): Promise<UserDto> {
-        try {
-            const request = await axios.get(
-                `${API_BASE_URL.USERS}/users/email/${email}`
-            );
-            return request.data;
-        } catch (error) {
-            console.log(
-                `Errro ao retornar usuario pelo email: ${error}`
-            );
-            throw error;
-        }        
-    }
+  async findUserByEmail(email: string): Promise<UserDto> {
+    const request = await axios
+      .get<UserDto>(`${API_BASE_URL.USERS}/users/email/${email}`)
+      .catch((x) => {
+        throw x.response.data;
+      });
+    return request.data;
+  }
 
-    async createUser(user: UserDto): Promise<void> {
-        try {
-            await axios.post(
-                `${API_BASE_URL.USERS}/users`,
-                user
-            );
-        } catch (error) {
-            console.log(
-                `Erro na criação de usuario: ${error}`
-            );
-            throw error;
-        }
+  async createUser(user: UserDto): Promise<void> {
+    try {
+      await axios.post(`${API_BASE_URL.USERS}/users`, user);
+    } catch (error) {
+      console.log(`Erro na criação de usuario: ${error}`);
+      throw error;
     }
-    
-    async updateUser(id: string, user: User): Promise<boolean> {
-        const request = await axios.put(
-            `${API_BASE_URL.USERS}/users/${id}`,
-            user
-        ).catch(x => { 
-            throw x.response.data;
-        });
-        return request.data;
-    }
+  }
 
-    async deleteUser(document: string): Promise<void> {
-        await axios.delete(
-            `${API_BASE_URL.USERS}/users/${document}`
-        ).catch(x => { 
-            throw x.response.data;
-        });
-    }
+  async updateUser(id: string, user: User): Promise<boolean> {
+    const request = await axios
+      .put(`${API_BASE_URL.USERS}/users/${id}`, user)
+      .catch((x) => {
+        throw x.response.data;
+      });
+    return request.data;
+  }
+
+  async deleteUser(document: string): Promise<void> {
+    await axios.delete(`${API_BASE_URL.USERS}/users/${document}`).catch((x) => {
+      throw x.response.data;
+    });
+  }
 }
